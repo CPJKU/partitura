9601dc505f838c4941b032e6234c042fd39b5d03
Maarten Grachten <maarten.grachten@gmail.com>
2019-02-18 11:48:27 +0100

    don't break when ply is not installed

--- ofaiutils/data_handling/__init__.py
+++ ofaiutils/data_handling/__init__.py
@@ -19,7 +19,6 @@ from . import scoreontology
 # Utilities
 from . import annotation_parser
 from . import instrument_assignment
-from . import ply_annotation_parser
 from . import sparse_datafiles
 from . import sparse_feature_extraction
 
--- ofaiutils/data_handling/annotation_parser.py
+++ ofaiutils/data_handling/annotation_parser.py
@@ -11,7 +11,8 @@ try:
     # serious). If you want to work on the ply annotation parser, you should set the
     # log level to a lower value.
     logging.getLogger('ply').setLevel(logging.ERROR)
-except ImportError:
+# except ImportError:
+except ModuleNotFoundError:
     LOGGER.warning('One or more packages needed for annotation parsing were not found: Using a dummy parser')
 
     from .scoreontology import Words
