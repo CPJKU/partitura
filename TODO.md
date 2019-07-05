6bd100b09a097cebac8eef6f58c575046caf257f
Maarten Grachten <maarten.grachten@ofai.at>
2019-07-05 11:38:07 +0200

    remove ofaiutils folder; include TODO.md

---
+++ TODO.md
@@ -0,0 +1,8 @@
+TODO
+====
+
+  - load MIDI to ScorePart
+    - pitch spelling (implement PS13?)
+  - export ScorePart to MIDI
+  - export ScorePart to MusicXML
+
--- ofaiutils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Data utilities for parsing MusicXML, Match, MIDI and Worm files
-"""
-from . import data_handling
-from . import utils
-
-__version__ = "0.0.1"
