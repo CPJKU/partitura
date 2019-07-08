5903652bb1f490d62fda6bcd66ce3bd5ee4181a3
Maarten Grachten <maarten.grachten@ofai.at>
2019-07-08 12:45:32 +0200

    add __init__.py file for tests

---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+# pylint: skip-file
+"""
+This module contains tests.
+"""
+
+import os
+
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+DATA_PATH = os.path.join(BASE_PATH, 'data')
+MUSICXML_PATH = os.path.join(DATA_PATH, 'musicxml')
