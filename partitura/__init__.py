1346ac5f345aaed6cf332887122b346763b2a28b
Maarten Grachten <maarten.grachten@ofai.at>
2019-08-07 17:42:18 +0200

    simplify import logic for directionparser

--- partitura/__init__.py
+++ partitura/__init__.py
@@ -1,4 +1,4 @@
-from .musicxmlold import load_musicxml as load_musicxml_old
+# from .musicxmlold import load_musicxml as load_musicxml_old
 from .musicxml import load_musicxml
 from .exportmusicxml import to_musicxml
 from .midi import load_midi
