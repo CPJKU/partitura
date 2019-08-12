9cf040651384499e14375a6e7ccb84c601cb2488
Maarten Grachten <maarten.grachten@ofai.at>
2019-08-12 19:06:37 +0200

    minor

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -5,9 +5,9 @@ This file contains test functions for the partitura.musicxml module.
 """
 
 import unittest
-import os
 
 from . import MUSICXML_PATH, MUSICXML_IMPORT_EXPORT_TESTFILES
+
 from partitura.directions import parse_words
 from partitura import load_musicxml, to_musicxml
 import partitura.score as score
@@ -23,7 +23,7 @@ class TestDirectionParser(unittest.TestCase):
         ('ligato', score.ConstantArticulationDirection),
         ('sempre cresc', score.DynamicLoudnessDirection),
         ('poco a poco rallentando', score.DynamicTempoDirection),
-        ('not a direction', score.Words)
+        ('this is not a direction', score.Words)
         ]
         
     def test_parser(self):
