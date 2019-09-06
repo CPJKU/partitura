d568fa2d3b6ce4999b10010ffbdf5759547ac6a6
Maarten Grachten <maarten.grachten@ofai.at>
2019-09-06 22:01:50 +0200

    rename file

--- partitura/__init__.py
+++ partitura/__init__.py
@@ -4,7 +4,7 @@
 
 from .importmusicxml import load_musicxml
 from .exportmusicxml import save_musicxml
-from .midi import load_midi
+from .importmidi import load_midi
 from .directions import parse_words
 
 # __all__ = ['load_musicxml', 'parse_words']
