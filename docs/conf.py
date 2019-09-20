d4e1b42333d929145d6b219828c989aacfcc5928
Maarten Grachten <maarten.grachten@ofai.at>
2019-09-20 19:23:49 +0200

    update sys.path in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 import pkg_resources
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../partitura'))
 
 # -- Project information -----------------------------------------------------
 
