74a7358437cc5549201017b39b76d7f114cbf82b
Maarten Grachten <maarten.grachten@ofai.at>
2019-10-26 20:25:39 +0200

    musescore display backend support; needs more testing across platforms

--- partitura/display.py
+++ partitura/display.py
@@ -9,6 +9,7 @@ application.
 import platform
 import logging
 import os
+import shutil
 import subprocess
 from tempfile import NamedTemporaryFile, TemporaryFile
 
@@ -16,6 +17,11 @@ from partitura import save_musicxml
 LOGGER = logging.getLogger(__name__)
 
 __all__ = ['show']
+
+def mscore_install_msg():
+    """Issue a platform specific installation suggestion for lilypond
+    """
+    pass
 
 def ly_install_msg():
     """Issue a platform specific installation suggestion for lilypond
@@ -34,11 +40,10 @@ def ly_install_msg():
              'http://lilypond.org/')
     return s
 
-
 def show(part, out_fmt='png'):
     """Show a rendering of one or more parts or partgroups using the
     desktop default application.
-
+    
     Parameters
     ----------
     part: :class:`partitura.score.Part` or :class:`partitura.score.PartGroup` or list of these
@@ -46,12 +51,78 @@ def show(part, out_fmt='png'):
     out_fmt: {'png', 'pdf'}, optional
         The image format of the rendered material
     """
+    
+    img_fn = show_musescore(part, out_fmt)
+    
+    if not os.path.exists(img_fn):
+        img_fn =  show_lilypond(part, out_fmt)
+        if not os.path.exists(img_fn):
+            return None
+    
+    # NOTE: the temporary image file will not be deleted.
+    if platform.system() == 'Linux':
+        subprocess.call(['xdg-open', img_fn])
+    elif platform.system() == 'Darwin':
+        subprocess.call(['open', img_fn])
+    elif platform.system() == 'Windows':
+        os.startfile(img_fn)
+
+def find_musescore3():
+    # for p in os.environ['PATH'].split(':'): 
+    #     c = glob.glob(os.path.join(p, 'MuseScore*')) 
+    #     if c: 
+    #         print(c) 
+    #         break 
+            
+    result = shutil.which('musescore')
+
+    if result is None:
+        result = shutil.which('mscore')
+
+    if platform.system() == 'Linux':
+        pass
+
+    elif platform.system() == 'Darwin':
+
+        result = shutil.which('/Applications/MuseScore 3.app/Contents/MacOS/mscore')
+
+    elif platform.system() == 'Windows':
+        pass
+
+    return result
+        
+def show_musescore(part, out_fmt):
+    mscore_exec = find_musescore3()
+    if not mscore_exec:
+        return False
+    if out_fmt not in ('png', 'pdf'):
+        print('warning: unsupported output format')
+        return
+    
+    with NamedTemporaryFile(suffix='.musicxml') as xml_fh, \
+         NamedTemporaryFile(suffix='.{}'.format(out_fmt), delete=False) as img_fh:
+        save_musicxml(part, xml_fh)
+        cmd = [mscore_exec, '-T', '10', '-r', '90', '-o', img_fh.name, xml_fh.name]
+        try:
+            ps = subprocess.run(cmd, stdin=xml_fh, stdout=subprocess.PIPE)
+            if ps.returncode != 0:
+                LOGGER.error('Command {} failed with code {}'
+                             .format(cmd, ps.returncode))
+                return
+        except FileNotFoundError as f:
+            LOGGER.error('Executing "{}" returned  {}. {}'
+                         .format(' '.join(cmd), f, mscore_install_msg()))
+            return
+        name, ext = os.path.splitext(img_fh.name)
+        return '{}-1{}'.format(name, ext)
+    
+def show_lilypond(part, out_fmt='png'):
     if out_fmt not in ('png', 'pdf'):
         print('warning: unsupported output format')
         return
 
     prvw_sfx = '.preview.{}'.format(out_fmt)
-
+    
     with TemporaryFile() as xml_fh, \
          NamedTemporaryFile(suffix=prvw_sfx, delete=False) as img_fh:
 
@@ -76,8 +147,8 @@ def show(part, out_fmt='png'):
                              .format(cmd1, ps1.returncode))
                 return
         except FileNotFoundError as f:
-            LOGGER.error('Executing "{}" returned  {}. {}'
-                         .format(' '.join(cmd1), f, ly_install_msg()))
+            LOGGER.error('Executing "{}" returned  {}.'
+                         .format(' '.join(cmd1), f))
             return
         
         # convert lilypond format (read from pipe of ps1) to image, and save to
@@ -92,16 +163,17 @@ def show(part, out_fmt='png'):
                              .format(cmd2, ps2.returncode))
                 return
         except FileNotFoundError as f:
-            LOGGER.error('Executing "{}" returned {}. {}'
-                         .format(' '.join(cmd2), f, ly_install_msg()))
+            LOGGER.error('Executing "{}" returned {}.'
+                         .format(' '.join(cmd2), f))
             return
 
-        # open the img file using the desktop default application
-        # NOTE: the temporary image file will not be deleted.
-        if platform.system() == 'Linux':
-            subprocess.call(['xdg-open', img_fh.name])
-        elif platform.system() == 'Darwin':
-            subprocess.call(['open', img_fh.name])
-        elif platform.system() == 'Windows':
-            os.startfile(img_fh.name)
+        return img_fh.name
+        # # open the img file using the desktop default application
+        # # NOTE: the temporary image file will not be deleted.
+        # if platform.system() == 'Linux':
+        #     subprocess.call(['xdg-open', img_fh.name])
+        # elif platform.system() == 'Darwin':
+        #     subprocess.call(['open', img_fh.name])
+        # elif platform.system() == 'Windows':
+        #     os.startfile(img_fh.name)
         
