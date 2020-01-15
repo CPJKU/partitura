5aee611fc9b746e2740d5aed22e06c7277b71aea
Nimrod <nimrodvarga@hotmail.com>
2020-01-15 15:27:57 +0100

    prototype handles chords now

--- partitura/io/exportmei_proto.py
+++ partitura/io/exportmei_proto.py
@@ -9,30 +9,32 @@ import sys
 part = score.Part("P0","Test")
 
 # test case blues lick
-# part.set_quarter_duration(0,10)
-# part.add(score.KeySignature(-3,"minor"),start=0)
-# part.add(score.TimeSignature(6,8),start=0)
-# part.add(score.Clef(sign="F",line=4, octave_change=0, number=1),start=0)
-# part.add(score.Clef(sign="G",line=2, octave_change=0, number=2),start=0)
-# n0 = score.Note(id="n0",step="C",octave=2,voice=1, staff=1)
-# n1 =score.Note(id="n1",step="E",octave=2,voice=1, staff=1, alter=-1)
-# n2 =score.Note(id="n2",step="D",octave=2,voice=1, staff=1)
-# part.add(n0,start=0,end=5)
-# part.add(n1,start=5,end=10)
-# part.add(n2,start=10,end=15)
-# n0q = score.Note(id="n0q",step="G",octave=2,voice=2, staff=1)
-# n1q =score.Note(id="n1q",step="G",octave=2,voice=2, staff=1)
-# part.add(n0q,start=0,end=10)
-# part.add(n1q,start=10,end=15)
-# part.add(score.Note(id="n3",step="C",octave=2,voice=1, staff=1),start=15,end=40)
-# n0s2 = score.Note(id="n0s2",step="C",octave=4,voice=1, staff=2)
-# n1s2 =score.Note(id="n1s2",step="E",octave=4,voice=1, staff=2, alter=-1)
-# n2s2 =score.Note(id="n2s2",step="D",octave=4,voice=1, staff=2)
-# part.add(n0s2,start=0,end=5)
-# part.add(n1s2,start=5,end=10)
-# part.add(n2s2,start=10,end=15)
-# part.add(score.Slur(n0s2,n2s2),start=0)
-# part.add(score.Note(id="n3s2",step="C",octave=4,voice=1, staff=2),start=15,end=40)
+part.set_quarter_duration(0,10)
+part.add(score.KeySignature(-3,"minor"),start=0)
+part.add(score.TimeSignature(6,8),start=0)
+part.add(score.Clef(sign="F",line=4, octave_change=0, number=1),start=0)
+part.add(score.Clef(sign="G",line=2, octave_change=0, number=2),start=0)
+n0 = score.Note(id="n0",step="C",octave=2,voice=1, staff=1)
+n1 =score.Note(id="n1",step="E",octave=2,voice=1, staff=1, alter=-1)
+n2 =score.Note(id="n2",step="D",octave=2,voice=1, staff=1)
+part.add(n0,start=0,end=5)
+part.add(n1,start=5,end=10)
+part.add(n2,start=10,end=15)
+n0q = score.Note(id="n0q",step="G",octave=2,voice=1, staff=1)
+n1q =score.Note(id="n1q",step="G",octave=2,voice=1, staff=1)
+n2q =score.Note(id="n2q",step="G",octave=2,voice=1, staff=1)
+part.add(n0q,start=0,end=5)
+part.add(n1q,start=5,end=10)
+part.add(n2q,start=10,end=15)
+part.add(score.Note(id="n3",step="C",octave=2,voice=1, staff=1),start=15,end=40)
+n0s2 = score.Note(id="n0s2",step="C",octave=4,voice=1, staff=2)
+n1s2 =score.Note(id="n1s2",step="E",octave=4,voice=1, staff=2, alter=-1)
+n2s2 =score.Note(id="n2s2",step="D",octave=4,voice=1, staff=2)
+part.add(n0s2,start=0,end=5)
+part.add(n1s2,start=5,end=10)
+part.add(n2s2,start=10,end=15)
+part.add(score.Slur(n0s2,n2s2),start=0)
+part.add(score.Note(id="n3s2",step="C",octave=4,voice=1, staff=2),start=15,end=40)
 
 
 # testing crossing measures and tieing notes together
@@ -44,7 +46,7 @@ part = score.Part("P0","Test")
 # part.add(score.Rest(id="r0",staff=1, voice=1),start=0,end=1)
 # part.add(score.Note(id="n0",step="C",octave=2, staff=1, voice=1),start=1,end=1+3*16+4+1)
 #
-# score.add_measures(part)
+score.add_measures(part)
 
 # using this feature?
 # making ties then becomes about looking at the tiegroup tag of notes
@@ -52,7 +54,7 @@ part = score.Part("P0","Test")
 # without this feature, notes crossing measure boundaries have to be handled
 #score.tie_notes(part)
 
-part = partitura.load_musicxml("../../tests/data_examples/Three-Part_Invention_No_13_(fragment).xml", force_note_ids=True)
+#part = partitura.load_musicxml("../../tests/data_examples/Three-Part_Invention_No_13_(fragment).xml", force_note_ids=True)
 
 
 
@@ -213,11 +215,21 @@ for m in part.iter_all(score.Measure):
 
 
                 for i in range(1,len(notes)):
+                    rep = chords[-1][0]
                     n = notes[i]
-
                     # what to do with notes that start same time but have different durations?
-                    if chords[-1][0].start.t==n.start.t and chords[-1][0].duration==n.duration:
-                        chords[-1].append(n)
+                    # should multiple Rests with same start time or a mix of Rests and Notes with same start times be ignored, trigger a warning or be an error?
+                    if isinstance(rep,score.Note) and isinstance(n, score.Note) and rep.start.t==n.start.t:
+                        if rep.duration==n.duration:
+                            chords[-1].append(n)
+                        else:
+                            print("In staff "+str(s)+",")
+                            print("in measure "+str(m.number)+",")
+                            print("for voice "+str(voice)+",")
+                            print("2 notes start at time "+str(n.start.t)+",")
+                            print("but have different durations, namely "+n.id+" has duration "+str(n.duration)+" and "+rep.id+" has duration "+str(rep.duration))
+                            print("change to same duration for a chord or change voice of one of the notes for something else")
+                            sys.exit()
                     else:
                         chords.append([n])
 
@@ -227,7 +239,6 @@ for m in part.iter_all(score.Measure):
 
                     if n.start.t+n.duration>m.end.t:
                         note_duration = m.end.t - n.start.t
-
                         # just some value different from None, for later checking
                         splitNote = True
 
@@ -293,136 +304,106 @@ for m in part.iter_all(score.Measure):
                     return dur_dots,splitNote
 
 
+                openBeam=False
+                parent = layer
+                for chordNotes in chords:
+                    rep = chordNotes[0]
+                    dur_dots,splitNote = calc_dur_dots_splitNote(rep)
 
-#                 for chordNotes in chords:
-#                     n = chordNotes[0]
-#                     dur_dots,splitNote = calc_dur_dots_splitNote(n)
-#
-#                     if isinstance(n,score.Note):
-#                         if splitNote!=None:
-#                             splitNote = score.Note(n.step,n.octave)
-#
-#
-#                         openBeam=False
-#                         parent = layer
-#                         if dur_dots[0][0]>8:
-#                             parent = addChild(layer,"beam")
-#                             openBeam = True
-#
-#                         if len(chordNotes)>1:
-#                             chord = addChild(parent,"chord")
-#                             setAttributes(chord,("dur",dur_dots[0][0]),("dots",dur_dots[0][1]))
-#
-#                             for n in chordNotes:
-#                                 note=addChild(chord,"note")
-#                                 setAttributes(note,(xmlIdString,n.id),("pname",n.step.lower()),("oct",n.octave))
-#                         else:
-#                             note=addChild(parent,"note")
-#                             setAttributes(note,(xmlIdString,n.id),("pname",n.step.lower()),("oct",n.octave),("dur",dur_dots[0][0]),("dots",dur_dots[0][1]))
-#
-#                         # additional ties will have to work together with ties specified in partitura
-#                         if len(dur_dots)>1:
-#                             # how to interleave beam and chords?
-#
-#                             for n in chordNotes:
-#
-#                                 t=[n.id]
-#                                 for i in range(1,len(dur_dots)):
-#                                     if not openBeam and dur_dots[i][0]>8:
-#                                         parent = addChild(layer,"beam")
-#                                         openBeam = True
-#
-#                                     note=addChild(parent,"note")
-#
-#                                     id = n.id+"-"+str(i)
-#
-#                                     t.append(id)
-#
-#                                     setAttributes(note,(xmlIdString,id),("pname",n.step.lower()),("oct",n.octave))
-#
-#
-#                                 ties[n.id]=t
-#
-#                         if splitNote!=None:
-#                             if len(dur_dots)>1:
-#                                 ties[n.id].append(n.id+"s")
-#                             else:
-#                                 ties[n.id]=[n.id, n.id+"s"]
+                    if isinstance(rep,score.Note):
+                        if not openBeam and dur_dots[0][0]>=8:
+                            parent = addChild(layer,"beam")
+                            openBeam = True
 
+                        if len(chordNotes)>1:
+                            chord = addChild(parent,"chord")
+                            setAttributes(chord,("dur",dur_dots[0][0]),("dots",dur_dots[0][1]))
 
-
-
-                for n in notes:
-                    dur_dots, splitNote = calc_dur_dots_splitNote(n)
-
-                    if isinstance(n,score.Note):
-                        if splitNote!=None:
-                            splitNote = score.Note(n.step,n.octave)
-
-
-                        openBeam=False
-                        parent = layer
-#                         if dur_dots[0][0]>8:
-#                             parent = addChild(layer,"beam")
-#                             openBeam = True
-
-                        note=addChild(parent,"note")
-
-
-
-                        setAttributes(note,(xmlIdString,n.id),("pname",n.step.lower()),("oct",n.octave),("dur",dur_dots[0][0]),("dots",dur_dots[0][1]))
+                            for n in chordNotes:
+                                note=addChild(chord,"note")
+                                setAttributes(note,(xmlIdString,n.id),("pname",n.step.lower()),("oct",n.octave))
+                        else:
+                            note=addChild(parent,"note")
+                            setAttributes(note,(xmlIdString,rep.id),("pname",rep.step.lower()),("oct",rep.octave),("dur",dur_dots[0][0]),("dots",dur_dots[0][1]))
 
                         # additional ties will have to work together with ties specified in partitura
                         if len(dur_dots)>1:
-                            t=[n.id]
+                            # how to interleave beam and chords?
+
+                            for n in chordNotes:
+                                ties[n.id]=[n.id]
+
                             for i in range(1,len(dur_dots)):
-                                if not openBeam and dur_dots[i][0]>8:
+                                if not openBeam and dur_dots[i][0]>=8:
                                     parent = addChild(layer,"beam")
                                     openBeam = True
 
-                                note=addChild(parent,"note")
+                                if len(chordNotes)>1:
+                                    chord = addChild(parent,"chord")
+                                    setAttributes(chord,("dur",dur_dots[i][0]),("dots",dur_dots[i][1]))
 
-                                id = n.id+"-"+str(i)
+                                    for n in chordNotes:
+                                        note=addChild(chord,"note")
 
-                                t.append(id)
+                                        id = n.id+"-"+str(i)
 
-                                setAttributes(note,(xmlIdString,id),("pname",n.step.lower()),("oct",n.octave),("dur",dur_dots[i][0]),("dots",dur_dots[i][1]))
+                                        ties[n.id].append(id)
+
+                                        setAttributes(note,(xmlIdString,id),("pname",n.step.lower()),("oct",n.octave))
 
 
-                            ties[n.id]=t
+                                else:
+                                    note=addChild(parent,"note")
+
+                                    id = rep.id+"-"+str(i)
+
+                                    ties[rep.id].append(id)
+
+                                    setAttributes(note,(xmlIdString,id),("pname",n.step.lower()),("oct",n.octave),("dur",dur_dots[i][0]),("dots",dur_dots[i][1]))
 
                         if splitNote!=None:
+                            splitNote = []
+
+                            for n in chordNotes:
+                                splitNote.append(score.Note(n.step,n.octave, id=n.id+"s"))
+
+
                             if len(dur_dots)>1:
-                                ties[n.id].append(n.id+"s")
+                                for n in chordNotes:
+                                    ties[n.id].append(n.id+"s")
                             else:
-                                ties[n.id]=[n.id, n.id+"s"]
+                                for n in chordNotes:
+                                    ties[n.id]=[n.id, n.id+"s"]
 
-                    elif isinstance(n,score.Rest):
+                    elif isinstance(rep,score.Rest):
                         if splitNote!=None:
-                            splitNote = score.Rest()
+                            splitNote = [score.Rest(id=rep.id+"s")]
 
                         rest = addChild(layer,"rest")
 
-                        setAttributes(rest,(xmlIdString,n.id),("dur",dur_dots[0][0]),("dots",dur_dots[0][1]))
+                        setAttributes(rest,(xmlIdString,rep.id),("dur",dur_dots[0][0]),("dots",dur_dots[0][1]))
 
                         if len(dur_dots)>1:
                             for i in range(1,len(dur_dots)):
                                 rest=addChild(layer,"rest")
 
-                                id = n.id+str(i)
+                                id = rep.id+str(i)
 
                                 setAttributes(rest,(xmlIdString,id),("dur",dur_dots[i][0]),("dots",dur_dots[i][1]))
 
                     if splitNote!=None:
-                        splitNote.voice = n.voice
-                        splitNote.start = m.end
-                        splitNote.end = score.TimePoint(n.start.t+n.duration)
-                        splitNote.id = n.id+"s"
+                        for sn in splitNote:
+                            sn.voice = rep.voice
+                            sn.start = m.end
+                            sn.end = score.TimePoint(rep.start.t+rep.duration)
 
-                        if s in notes_nextMeasure_perStaff.keys():
-                            notes_nextMeasure_perStaff[s].append(splitNote)
-                        else:
-                            notes_nextMeasure_perStaff[s]=[splitNote]
+                            if s in notes_nextMeasure_perStaff.keys():
+                                notes_nextMeasure_perStaff[s].append(sn)
+                            else:
+                                notes_nextMeasure_perStaff[s]=[sn]
+
+
+                # for now all notes are beamed, however some rules should be obeyed there, see Note Beaming and Grouping
 
 
                 ties_perStaff_perVoice[voice]=ties
