b364c4fa0e3c073fc0053da84ecf588d0ef8cdda
Silvan David Peter <silvan.peter@ofai.at>
2020-11-09 12:21:55 +0100

    nakamura test start

--- tests/test_nakamura.py
+++ tests/test_nakamura.py
@@ -37,29 +37,9 @@ class TestLoadNakamura(unittest.TestCase):
     def test_match_lines(self):
 
         snote_line = 'snote(1-1,[E,n],4,0:1,0,1/4,-1.0,0.0,[staff1])'
-        note_line = 'note(0,[E,n],4,471720,472397,472397,49)'
-        old_note_line = 'note(0,[E,n],4,471720,472397,49)'
-        snote_note_line = 'snote(1-1,[E,n],4,0:1,0,1/4,-1.0,0.0,[staff1])-note(0,[E,n],4,471720,472397,472397,49).'
-        snote_deletion_line = 'snote(1-1,[E,n],4,0:1,0,1/4,-1.0,0.0,[staff1])-deletion.'
-        note_insertion_line = 'insertion-' + note_line + '.'
-        info_line = 'info(matchFileVersion,4.0).'
-        meta_line = 'meta(keySignature,C Maj/A min,0,-1.0).'
-        sustain_line = 'sustain(779,59).'
-        trill_line = 'trill(726-1)-note(751,[D,n],5,57357,57533,57533,60).'
-        ornament_line = 'ornament(726-1)-note(751,[D,n],5,57357,57533,57533,60).'
 
-        matchlines = [snote_note_line,
-                      snote_deletion_line,
-                      note_insertion_line,
-                      info_line,
-                      meta_line,
-                      sustain_line,
-                      trill_line,
-                      ornament_line]
 
-        for ml in matchlines:
-            mo = parse_matchline(ml)
-            self.assertTrue(mo.matchline, ml)
+        self.assertTrue(mo.matchline, ml)
 
 
 if __name__ == '__main__':
