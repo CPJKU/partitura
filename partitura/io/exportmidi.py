21c3206ce6d2aa4ac3a3ce023e796fb3f2041d2c
Carlos Cancino-Chacón <carloscancinochacon@gmail.com>
2021-04-29 13:45:26 +0200

    flake8 cleanup io/exportmidi.py

--- partitura/io/exportmidi.py
+++ partitura/io/exportmidi.py
@@ -264,13 +264,12 @@ def save_score_midi(parts, out, part_voice_assign_mode=0, velocity=64):
 
         for note in notes:
 
-            # key is a tuple (part_group, part, voice) that will be converted into a (track, channel) pair.
+            # key is a tuple (part_group, part, voice) that will be
+            # converted into a (track, channel) pair.
             key = (pg, part, note.voice)
             events[key][to_ppq(note.start.t)].append(
                 Message("note_on", note=note.midi_pitch)
             )
-            # TODO: Fix issues with end_tied
-            # events[key][to_ppq(note.end_tied.t)].append(Message('note_off', note=note.midi_pitch))
             events[key][to_ppq(note.start.t + note.duration_tied)].append(
                 Message("note_off", note=note.midi_pitch)
             )
