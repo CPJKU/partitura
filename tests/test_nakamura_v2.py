65daad86f335e37328c688b3a4c109001249684a
Carlos Cancino-Chacón <carloscancinochacon@gmail.com>
2021-05-27 16:27:23 +0200

    add test for load_nakamuramatch

--- tests/test_nakamura_v2.py
+++ tests/test_nakamura_v2.py
@@ -1,24 +1,24 @@
 """
 
-This file contains test functions for the import of Nakamura et al.'s match and corresp file formats.
+This file contains test functions for the import of Nakamura et al.'s match and
+corresp file formats.
 
 """
 
 import unittest
 from partitura.io.importnakamura_v2 import (
-    load_nakamuracorresp_v2,
+    load_nakamuracorresp,
+    load_nakamuramatch
 )
 
 from . import NAKAMURA_IMPORT_TESTFILES
-
-# from partitura import load_nakamuracorresp, load_nakamuramatch
 
 
 class TestLoadNakamura(unittest.TestCase):
     def test_import(self):
         for fn in NAKAMURA_IMPORT_TESTFILES:
             if "corresp" in fn:
-                performance, score, align = load_nakamuracorresp_v2(fn)
+                performance, score, align = load_nakamuracorresp(fn)
                 performance_ids = set(
                     item["performance_id"]
                     for item in align
@@ -32,8 +32,19 @@ class TestLoadNakamura(unittest.TestCase):
                 self.assertTrue(len(performance_ids) == len(performance))
                 self.assertTrue(len(score_ids) == len(score))
             else:
-                # TODO: test match
-                pass
+                performance, score, align = load_nakamuramatch(fn)
+                performance_ids = set(
+                    item["performance_id"]
+                    for item in align
+                    if item["label"] in ("match", "insertion")
+                )
+                score_ids = set(
+                    item["score_id"]
+                    for item in align
+                    if item["label"] in ("match", "deletion")
+                )
+                self.assertTrue(len(performance_ids) == len(performance))
+                self.assertTrue(len(score_ids) == len(score))
 
 
 if __name__ == "__main__":
