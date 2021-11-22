bb5c2757801502b055f4a1db425fc6194b024b82
Emmanouil Karystinaios <emmanouil.karystinaios@jku.at>
2021-11-22 12:04:23 +0100

    added tests for key 2 mode computations.

--- tests/test_key_estimation.py
+++ tests/test_key_estimation.py
@@ -6,6 +6,21 @@ from partitura.musicanalysis import estimate_key
 from partitura.utils import key_name_to_fifths_mode, fifths_mode_to_key_name
 
 
+class KeyModeComputation(unittest.TestCase):
+    def test_key_name_to_fifths_mode(self):
+        self.assertEqual((0, 'minor'), key_name_to_fifths_mode('Am'))
+        self.assertEqual((0, 'major'), key_name_to_fifths_mode('C'))
+        self.assertEqual((3, 'major'), key_name_to_fifths_mode('A'))
+        self.assertEqual((10, "major"), key_name_to_fifths_mode("A#"))
+        self.assertEqual((8, "minor"), key_name_to_fifths_mode("E#m"))
+
+    def test_fifths_modes_to_key_name(self):
+        self.assertEqual("Am", fifths_mode_to_key_name(0, 'minor'))
+        self.assertEqual("C", fifths_mode_to_key_name(0, 'major'))
+        self.assertEqual("A", fifths_mode_to_key_name(3, 'major'))
+        self.assertEqual("F", fifths_mode_to_key_name(-1, 1))
+
+
 class TestKeyEstimation(unittest.TestCase):
     """
     Test key estimation
@@ -21,16 +36,7 @@ class TestKeyEstimation(unittest.TestCase):
         key = estimate_key(self.score.note_array)
         self.assertTrue(key == "Am", "Incorrect key")
 
-    def key_name_to_fifths_mode(self):
-        self.assertEqual(key_name_to_fifths_mode('Am'), (0, 'minor'))
-        self.assertEqual(key_name_to_fifths_mode('C'), (0, 'major'))
-        self.assertEqual(key_name_to_fifths_mode('A'), (3, 'major'))
 
-    def fifths_modes_to_key_name(self):
-        self.assertEqual(fifths_mode_to_key_name(0, 'minor'), "Am")
-        self.assertEqual(fifths_mode_to_key_name(0, 'major'), 'C')
-        self.assertEqual(fifths_mode_to_key_name(3, 'major'), 'A')
-        self.assertEqual(fifths_mode_to_key_name(-1, 1), "F")
 
 
 if __name__ == "__main__":
