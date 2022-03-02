a79225cda5c2988e616ac69d63d83a61520dd53e
melkisedeath <manoskaristineos@gmail.com>
2022-03-02 10:19:27 +0100

    Adding measure method in kern.

--- partitura/io/importkern.py
+++ partitura/io/importkern.py
@@ -63,6 +63,7 @@ class KernParserPart(KernGlobalPart):
         self.position = init_pos
         self.parsing = "full"
         self.stream = stream
+        self.prev_measure = 0
         self.last_repeat_pos = None
         self.mode = None
         self.barline_dict = dict() if not barline_dict else barline_dict
@@ -130,6 +131,8 @@ class KernParserPart(KernGlobalPart):
         self.add(new_time_signature, self.position)
 
     def _handle_barline(self, element):
+        self.add(score.Measure, self.prev_measure, self.position)
+        self.prev_measure = self.position
         if len(element.split()) > 1:
             element = element.split()[0]
         if element.endswith("!") or element == "==":
