e50f781b835b43864ce7978774525ebb00c9a774
melkisedeath <manoskaristineos@gmail.com>
2022-03-04 12:20:09 +0100

    Included make_note_feats in musicanalysis init

--- partitura/musicanalysis/__init__.py
+++ partitura/musicanalysis/__init__.py
@@ -9,6 +9,7 @@ from .voice_separation import estimate_voices
 from .key_identification import estimate_key
 from .pitch_spelling import estimate_spelling
 from .tonal_tension import estimate_tonaltension
+from .note_features import make_note_feats
 
 __all__ = [
     "estimate_voices",
