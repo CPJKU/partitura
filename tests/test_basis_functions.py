1a3d3bc4b83f08377013f85b655551f7c7772561
melkisedeath <manoskaristineos@gmail.com>
2022-03-07 12:40:26 +0100

    Additional note_feature testing.

--- tests/test_basis_functions.py
+++ tests/test_basis_functions.py
@@ -1,13 +1,24 @@
 import unittest
-from tests import METRICAL_POSITION_TESTFILES
-from partitura import load_musicxml
+from tests import METRICAL_POSITION_TESTFILES, MUSICXML_IMPORT_EXPORT_TESTFILES, MEI_TESTFILES
+from partitura import load_musicxml, load_mei
 from partitura.musicanalysis import make_note_feats
+
 
 class TestingNoteFeatureExtraction(unittest.TestCase):
     def test_metrical_basis(self):
         for fn in METRICAL_POSITION_TESTFILES:
             part = load_musicxml(fn)
-            basis, names = make_note_feats(part, ['metrical_basis'])
+            make_note_feats(part, ['metrical_basis'])
+
+    def test_grace_basis(self):
+        fn = [f for f in MEI_TESTFILES if f.endswith("test_grace_note.mei")][0]
+        part = load_mei(fn)
+        make_note_feats(part, ['grace_basis'])
+
+    def test_all_basis(self):
+        for fn in MUSICXML_IMPORT_EXPORT_TESTFILES:
+            part = load_musicxml(fn)
+            make_note_feats(part, "all")
 
 
 if __name__ == '__main__':
