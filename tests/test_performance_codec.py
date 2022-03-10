4a2e5941446347a159a09afaf64e369abc1cfc22
melkisedeath <manoskaristineos@gmail.com>
2022-03-10 15:42:34 +0100

    Tests for performance encode-decode.

--- tests/test_performance_codec.py
+++ tests/test_performance_codec.py
@@ -3,18 +3,21 @@
 This file contains test functions for Performance Array Calculations
 
 """
-import logging
 import unittest
-from tempfile import TemporaryFile
-
+import numpy as np
 from tests import MATCH_IMPORT_EXPORT_TESTFILES
 from partitura import load_match
-from partitura.musicanalysis.performance_codec import PerformanceCodec
+from partitura.musicanalysis import performance_encode, performance_decode
+
 
 class TestPerformanceCoded(unittest.TestCase):
-    def test_encode(self):
-        pc = PerformanceCodec()
+    def test_encode_decode(self):
         for fn in MATCH_IMPORT_EXPORT_TESTFILES:
             ppart, alignment, spart = load_match(fn, create_part=True)
-            performed_array, _ = pc.encode(spart, ppart, alignment)
-            print(performed_array[:10])
+
+            performance_array, _ = performance_encode(spart, ppart, alignment)
+            decoded_ppart, decoded_alignment = performance_decode(spart, performance_array, return_alignment=True)
+            # normalize ppart notearray onset sec starting from 0.
+            orig_sec_array = ppart.note_array["onset_sec"] - ppart.note_array["onset_sec"].min()
+            target = np.all(np.allclose(np.sort(decoded_ppart.note_array["onset_sec"]), np.sort(orig_sec_array)))
+            self.assertTrue(target, "The decoded Performed Part doesn't match the original.")
