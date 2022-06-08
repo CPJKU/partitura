c9a503cce8c3d3f9ac92ed74b0df79347efca1fb
melkisedeath <manoskaristineos@gmail.com>
2022-06-08 16:32:15 +0200

    Note array to part test and corrections.

--- partitura/musicanalysis/__init__.py
+++ partitura/musicanalysis/__init__.py
@@ -11,7 +11,7 @@ from .pitch_spelling import estimate_spelling
 from .tonal_tension import estimate_tonaltension
 from .note_features import make_note_feats, compute_note_array, full_note_array, make_rest_feats, make_rest_features
 from .performance_codec import encode_performance, decode_performance
-
+from .note_array_to_part import note_array_to_part
 
 __all__ = [
     "estimate_voices",
