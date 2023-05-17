8c9d1bdcf6cecdc82f09362df1b4610be9be520e
Huan Zhang <31784445+Kristin33@users.noreply.github.com>
2023-05-17 11:13:58 +0200

    pedal features added

--- partitura/musicanalysis/performance_features.py
+++ partitura/musicanalysis/performance_features.py
@@ -14,9 +14,10 @@ np.set_printoptions(suppress=True)
 import matplotlib.pyplot as plt
 from scipy import stats
 from scipy.optimize import least_squares
+from scipy.signal import find_peaks
 import numpy.lib.recfunctions as rfn
 from partitura.score import ScoreLike
-from partitura.performance import PerformanceLike
+from partitura.performance import PerformanceLike, PerformedPart
 from partitura.utils.generic import interp1d
 from partitura.musicanalysis.performance_codec import to_matched_score, onsetwise_to_notewise, encode_tempo
 
@@ -54,7 +55,7 @@ def compute_performance_features(score: ScoreLike,
         the functions themselves or the names of a feature function as
         strings (or a mix). 
         currently implemented: 
-        asynchrony_feature, articulation_feature, dynamics_feature
+        asynchrony_feature, articulation_feature, dynamics_feature, pedal_feature
 
     Returns
     -------
@@ -236,7 +237,7 @@ def parse_changing_ramp(unique_onset_idxs, m_score):
 
 
 ### Asynchrony
-def asynchrony_feature(m_score: list,
+def asynchrony_feature(m_score: np.ndarray,
                      unique_onset_idxs: list,
                      performance: PerformanceLike,
                      v=False):
@@ -245,10 +246,12 @@ def asynchrony_feature(m_score: list,
 
     Parameters
     ----------
-    unique_onset_idxs : list
-        a list of arrays with the note indexes that have the same onset
     m_score : list
         correspondance between score and performance notes, with score markings. 
+    unique_onset_idxs : list
+        a list of arrays with the note indexes that have the same onset
+    performance: PerformedPart
+        The original PerformedPart object
     
     Returns
     -------
@@ -293,7 +296,7 @@ def asynchrony_feature(m_score: list,
 
 
 ### Dynamics 
-def dynamics_feature(m_score : list,
+def dynamics_feature(m_score : np.ndarray,
                         unique_onset_idxs: list,
                         performance: PerformanceLike,
                         window : int = 3,
@@ -303,8 +306,12 @@ def dynamics_feature(m_score : list,
 
     Parameters
     ----------
-    m_score : structured array
+    m_score : list
         correspondance between score and performance notes, with score markings. 
+    unique_onset_idxs : list
+        a list of arrays with the note indexes that have the same onset
+    performance: PerformedPart
+        The original PerformedPart object
     window : int
         Length of window to look at the range of dynamic marking coverage, default 3. 
     agg : string
@@ -378,7 +385,7 @@ def dynamics_feature(m_score : list,
 
 ### Articulation
 
-def articulation_feature(m_score : list, 
+def articulation_feature(m_score : np.ndarray, 
                          unique_onset_idxs: list,
                          performance: PerformanceLike,
                          return_mask=False):
@@ -390,8 +397,12 @@ def articulation_feature(m_score : list,
 
     Parameters
     ----------
-    m_score : structured array
+    m_score : list
         correspondance between score and performance notes, with score markings. 
+    unique_onset_idxs : list
+        a list of arrays with the note indexes that have the same onset
+    performance: PerformedPart
+        The original PerformedPart object
     return_mask : bool
         if true, return a boolean mask of legato notes, staccato notes and repeated notes
 
@@ -473,30 +484,6 @@ def get_next_note(note_info, match_voiced):
 
 ### Pedals 
 
-def pedal_ramp(ppart: PerformedPart):
-    """Pedal ramp.
-
-    Returns:
-    * pramp : a ramp function that ranges from 0
-                  to 127 with the change of sustain pedal
-    """
-    pedal_controls = ppart.controls
-    W = np.zeros((len(onsets), 2))
-
-    for slur in slurs:
-        if not slur.end:
-            continue
-        x = [slur.start.t, slur.end.t]
-        y_inc = [0, 1]
-        y_dec = [1, 0]
-        W[:, 0] += interp1d(x, y_inc, bounds_error=False, fill_value=0)(onsets)
-        W[:, 1] += interp1d(x, y_dec, bounds_error=False, fill_value=0)(onsets)
-
-    # Filter out NaN values
-    W[np.isnan(W)] = 0.0
-    return W
-
-
 def pedal_feature(m_score : list, 
                 unique_onset_idxs: list,
                 performance: PerformanceLike):
@@ -505,39 +492,69 @@ def pedal_feature(m_score : list,
 
     Parameters
     ----------
-    m_score : structured array
+    m_score : list
         correspondance between score and performance notes, with score markings. 
+    unique_onset_idxs : list
+        a list of arrays with the note indexes that have the same onset
+    performance: PerformedPart
+        The original PerformedPart object
         
     Returns
     -------
     pedal_ : structured array (4, n_notes) with fields
-        onset_value [0, 127]: 
-        offset_value [0, 127]:
-        prev_release_time :
-        next_release_time: 
+        onset_value [0, 127]: The interpolated pedal value at the onset
+        offset_value [0, 127]: The interpolated pedal value at the onset
+        to_prev_release [0, 10]: delta time from note onset to the previous pedal release 'peak'
+        to_next_release [0, 10]: delta time from note offset to the next pedal release 'peak'
     """  
-    pedal_ramp(performance.performedparts[0])
-    hook()
-    kor_ = np.full(len(m_score), -1, dtype=[("kor", "f4")])
-
-    # consider the note transition by each voice
-    for voice in np.unique(m_score['voice']):
-        match_voiced = m_score[m_score['voice'] == voice]
-        for _, note_info in enumerate(match_voiced):
-
-            if note_info['onset'] == match_voiced['onset'].max():  # last beat
-                break
-            next_note_info = get_next_note(note_info, match_voiced) # find most plausible transition
-
-            if next_note_info: # in some cases no meaningful transition
-                j = np.where(m_score == note_info)[0].item()  # original position
-
-                if (note_info['offset'] == next_note_info['onset']):
-                    kor_[j]['kor'] =  get_kor(note_info, next_note_info)
-
     
+    onset_offset_pedals, ramp_func = pedal_ramp(performance.performedparts[0], m_score)
 
-    return kor_
+    x = np.linspace(0, 100, 200)
+    y = ramp_func(x)
+
+    peaks, _ = find_peaks(-y, prominence=10)
+    peak_timepoints = x[peaks]
+
+    release_times = np.zeros(len(m_score), dtype=[("to_prev_release", "f4"), ("to_next_release", "f4")])
+    for i, note in enumerate(m_score):
+        peaks_before = peak_timepoints[note['p_onset'] >= peak_timepoints]
+        peaks_after = peak_timepoints[(note['p_onset'] + note['p_duration']) <= peak_timepoints]
+        if len(peaks_before):
+            release_times[i]["to_prev_release"] = min(note['p_onset'] - peaks_before.max(), 10)
+        if len(peaks_after):
+            release_times[i]["to_next_release"] = min(peaks_after.min() - (note['p_onset'] + note['p_duration']), 10)
+
+    # plt.plot(x[peaks], y[peaks], "x")
+    # plt.plot(x, y)
+    # plt.show()
+
+    return rfn.merge_arrays([onset_offset_pedals, release_times], flatten=True, usemask=False)
+
+def pedal_ramp(ppart: PerformedPart,
+               m_score: np.ndarray):
+    """Pedal ramp in the same shape as the m_score.
+
+    Returns:
+    * pramp : a ramp function that ranges from 0
+                  to 127 with the change of sustain pedal
+    """
+    pedal_controls = ppart.controls
+    W = np.zeros((len(m_score), 2))
+    onset_timepoints = m_score['p_onset']
+    offset_timepoints = m_score['p_onset'] + m_score['p_duration']
+
+    timepoints = [control['time'] for control in pedal_controls]
+    values = [control['value'] for control in pedal_controls]
+
+    agg_ramp_func = interp1d(timepoints, values, bounds_error=False, fill_value=0)
+    W[:, 0] = agg_ramp_func(onset_timepoints)
+    W[:, 1] = agg_ramp_func(offset_timepoints)
+
+    # Filter out NaN values
+    W[np.isnan(W)] = 0.0
+
+    return np.array([tuple(i) for i in W], dtype=[("onset_value", "f4"), ("offset_value", "f4")]), agg_ramp_func
 
 
 
