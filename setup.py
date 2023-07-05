eee1b424645bf239246c71d5f29aac53738f33cc
sildater <41552783+sildater@users.noreply.github.com>
2023-07-05 15:36:02 +0200

    Update setup.py version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ URL = "https://github.com/CPJKU/partitura"
 EMAIL = "partitura-users@googlegroups.com"
 AUTHOR = "Maarten Grachten, Carlos Cancino-Chacón, Silvan Peter, Emmanouil Karystinaios, Francesco Foscarin, Thassilo Gadermaier"
 REQUIRES_PYTHON = ">=3.7"
-VERSION = "1.3.0"
+VERSION = "1.3.1"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["numpy", "scipy", "lxml", "lark-parser", "xmlschema", "mido"]
