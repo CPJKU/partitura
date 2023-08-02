4e883e73445e71ea11073d8241d0504ff10cdd50
fosfrancesco <foscarin.francesco@gmail.com>
2023-08-02 16:54:04 +0200

    Removed Musescore Tests, because they can't run on github

--- tests/test_musescore.py
+++ tests/test_musescore.py
@@ -19,15 +19,12 @@ import platform
 import numpy as np
 from pathlib import Path
 
+# This tests are commented, because it is not possible to run MuseScore on a Github
 
-class TestImportMusescore(unittest.TestCase):
-    def test_epfl_scores(self):
-        # dirty trick, since we can install Musescore only on linux environment in github actions
-        if platform.system() == "Linux":
-            score = load_via_musescore(MUSESCORE_TESTFILES[0])
-            self.assertTrue(len(score.parts) == 1)
-            # try the generic loading function
-            score = load_score(MUSESCORE_TESTFILES[1])
-            self.assertTrue(len(score.parts) == 1)
-        else:
-            self.skipTest("MuseScore test can't run on non-linux environment in github actions")
+# class TestImportMusescore(unittest.TestCase):
+#     def test_epfl_scores(self):
+#         score = load_via_musescore(MUSESCORE_TESTFILES[0])
+#         self.assertTrue(len(score.parts) == 1)
+#         # try the generic loading function
+#         score = load_score(MUSESCORE_TESTFILES[0])
+#         self.assertTrue(len(score.parts) == 1)
