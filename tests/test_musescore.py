69bfe5a4203bf045a0426fbc08ea1b224c06556d
huispaty <hu.patriciaziyi@hotmail.com>
2023-09-22 17:24:46 +0200

    fixed  musescore testing

--- tests/test_musescore.py
+++ tests/test_musescore.py
@@ -13,20 +13,21 @@ from partitura.io.importmei import MeiParser
 from partitura.utils import compute_pianoroll
 from lxml import etree
 from xmlschema.names import XML_NAMESPACE
-from partitura.io import load_score, load_via_musescore, find_musescore, MuseScoreNotFoundException
+from partitura.io import load_score, load_via_musescore
+from partitura.io.musescore import find_musescore, MuseScoreNotFoundException
 import platform
 
 import numpy as np
 from pathlib import Path
 
 try:
-  find_musescore()
-  class TestImportMusescore(unittest.TestCase):
-      def test_epfl_scores(self):
-          score = load_via_musescore(MUSESCORE_TESTFILES[0])
-          self.assertTrue(len(score.parts) == 1)
-          # try the generic loading function
-          score = load_score(MUSESCORE_TESTFILES[0])
-          self.assertTrue(len(score.parts) == 1)
+  if find_musescore():
+    class TestImportMusescore(unittest.TestCase):
+        def test_epfl_scores(self):
+            score = load_via_musescore(MUSESCORE_TESTFILES[0])
+            self.assertTrue(len(score.parts) == 1)
+            # try the generic loading function
+            score = load_score(MUSESCORE_TESTFILES[0])
+            self.assertTrue(len(score.parts) == 1)
 except MuseScoreNotFoundException:
     pass
