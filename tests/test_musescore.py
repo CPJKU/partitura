0fce07679fa0d4d4ec9a14152bceb2a5c32b6920
Patricia Hu <66968965+huispaty@users.noreply.github.com>
2023-09-22 17:13:08 +0200

    Update test_musescore.py

--- tests/test_musescore.py
+++ tests/test_musescore.py
@@ -13,19 +13,20 @@ from partitura.io.importmei import MeiParser
 from partitura.utils import compute_pianoroll
 from lxml import etree
 from xmlschema.names import XML_NAMESPACE
-from partitura.io import load_score, load_via_musescore
+from partitura.io import load_score, load_via_musescore, find_musescore, MuseScoreNotFoundException
 import platform
 
 import numpy as np
 from pathlib import Path
 
-# This tests are commented, because it is not possible to run MuseScore on Github server
-# uncomment to run them locally instead
-
-# class TestImportMusescore(unittest.TestCase):
-#     def test_epfl_scores(self):
-#         score = load_via_musescore(MUSESCORE_TESTFILES[0])
-#         self.assertTrue(len(score.parts) == 1)
-#         # try the generic loading function
-#         score = load_score(MUSESCORE_TESTFILES[0])
-#         self.assertTrue(len(score.parts) == 1)
+try:
+  find_musescore()
+  class TestImportMusescore(unittest.TestCase):
+      def test_epfl_scores(self):
+          score = load_via_musescore(MUSESCORE_TESTFILES[0])
+          self.assertTrue(len(score.parts) == 1)
+          # try the generic loading function
+          score = load_score(MUSESCORE_TESTFILES[0])
+          self.assertTrue(len(score.parts) == 1)
+except MuseScoreNotFoundException:
+    pass
