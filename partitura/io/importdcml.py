84d01c654708660158977f3e98b2db4d6a392b47
melkisedeath <manoskaristineos@gmail.com>
2024-01-17 14:37:31 +0100

    added support for tied notes.

--- partitura/io/importdcml.py
+++ partitura/io/importdcml.py
@@ -36,46 +36,60 @@ def read_note_tsv(note_tsv_path, metadata=None):
     note_array = data[["onset_div", "duration_div", "pitch", "step", "alter", "octave", "id", "staff", "voice"]].to_records(index=False)
     part = spt.Part("P0", "Metadata", quarter_duration=qdivs)
 
-    # Add notes
-    notes = note_array[~grace_mask]
-    for note in notes:
-        symbolic_duration = estimate_symbolic_duration(note["duration_div"], qdivs)
-        part.add(
-            spt.Note(
+    # Add notes and grace notes
+    for n_idx, note in enumerate(note_array):
+        if grace_mask[n_idx]:
+            # verify that staff and voice are the same for the grace note and the main note
+            note_el = spt.GraceNote(
+                grace_type="grace",
                 id="n-{}".format(note["id"]),
                 step=note["step"],
                 octave=note["octave"],
                 alter=note["alter"],
                 staff=note["staff"],
                 voice=note["voice"],
-                symbolic_duration=symbolic_duration
-            ), start=note["onset_div"], end=(note["onset_div"]+note["duration_div"]))
-    # Add Grace notes
+                symbolic_duration={"type": "eighth"},
+            )
+
+            if grace_mask[n_idx - 1]:
+                prev_note = note_array[n_idx - 1]
+                for note_prev in part.iter_all(spt.GraceNote, note["onset_div"], note["onset_div"] + 1):
+                    if note_prev.id == "n-{}".format(prev_note["id"]):
+                        note_el.grace_prev = note_prev
+                        note_prev.grace_next = note_el
+                        break
+        else:
+            symbolic_duration = estimate_symbolic_duration(note["duration_div"], qdivs)
+            note_el = spt.Note(
+                    id="n-{}".format(note["id"]),
+                    step=note["step"],
+                    octave=note["octave"],
+                    alter=note["alter"],
+                    staff=note["staff"],
+                    voice=note["voice"],
+                    symbolic_duration=symbolic_duration)
+        part.add(note_el, start=note["onset_div"], end=(note["onset_div"]+note["duration_div"]))
+
+    # Curate grace notes
     grace_note_idxs = np.where(grace_mask)[0]
-    for grace_idx in grace_note_idxs:
-        grace_note = note_array[grace_idx]
-        grace_el = spt.GraceNote(
-            grace_type="grace",
-            id="n-{}".format(grace_note["id"]),
-            step=grace_note["step"],
-            octave=grace_note["octave"],
-            alter=grace_note["alter"],
-            staff=grace_note["staff"],
-            voice=grace_note["voice"],
-            symbolic_duration={"type": "eighth"},
-        )
-
-        if grace_mask[grace_idx-1]:
-            prev_note = note_array[grace_idx-1]
-            for note in part.iter_all(spt.GraceNote, grace_note["onset_div"], grace_note["onset_div"]+1):
-                if note.id == "n-{}".format(prev_note["id"]):
-                    grace_el.grace_prev = note
-                    note.grace_next = grace_el
+    for i, grace_el in enumerate(part.iter_all(spt.GraceNote)):
+        grace_idx = grace_note_idxs[i]
+        note = note_array[grace_idx]
+        # Find the next note in the same staff and voice
+        if not grace_mask[grace_idx+1]:
+            i = 1
+            next_note = note_array[grace_idx+i]
+            while note["staff"] != next_note["staff"] or note["voice"] != next_note["voice"]:
+                i += 1
+                next_note = note_array[grace_idx+i]
+            assert note["staff"] == next_note["staff"], "Grace note and main note must be in the same staff"
+            assert note["voice"] == next_note["voice"], "Grace note and main note must be in the same voice"
+            assert note["onset_div"] == next_note[
+                "onset_div"], "Grace note and main note must have the same onset"
+            for note in part.iter_all(spt.Note, note["onset_div"], note["onset_div"]+1):
+                if note.id == "n-{}".format(next_note["id"]):
+                    grace_el.grace_next = note
                     break
-
-        part.add(
-            grace_el, grace_note["onset_div"], grace_note["onset_div"]
-        )
 
     # Find time signatures
     time_signatures_changes = data["timesig"][data["timesig"].shift(1) != data["timesig"]].index
@@ -89,8 +103,19 @@ def read_note_tsv(note_tsv_path, metadata=None):
     # Add default clefs for piano pieces (Naive)
     part.add(spt.Clef(staff=1, sign="G", line=2, octave_change=0), start=0)
     part.add(spt.Clef(staff=2, sign="F", line=4, octave_change=0), start=0)
+
     # TODO: Find Ties
-    tied_notes = data["tied"].dropna()
+    tied_note_mask = data["tied"] == 1
+    for tied_note in note_array[tied_note_mask]:
+        for note in part.iter_all(spt.Note, tied_note["onset_div"], tied_note["onset_div"]+1):
+            if note.id == "n-{}".format(tied_note["id"]):
+                for note_next in part.iter_all(spt.Note, note.end.t, note.end.t+1, mode="starting"):
+                    if note_next.alter == note.alter and note_next.step == note.step and note_next.octave == note.octave:
+                        assert note_next.voice == note.voice, "Tied notes must be in the same voice"
+                        assert note_next.staff == note.staff, "Tied notes must be in the same staff"
+                        note.tie_next = note_next
+                        note_next.tie_prev = note
+                        break
 
     return part
 
@@ -103,7 +128,7 @@ def read_measure_tsv(measure_tsv_path, part):
     repeat_index = 0
 
     for idx, row in data.iterrows():
-        part.add(spt.Measure(), start=row["onset_div"], end=row["onset_div"]+row["duration_div"])
+        part.add(spt.Measure(number=row["mc"], name=row["mn"]), start=row["onset_div"], end=row["onset_div"]+row["duration_div"])
         # if row["repeat"] == "start":
         if row["repeats"] == "start":
             repeat_index = idx
@@ -111,6 +136,8 @@ def read_measure_tsv(measure_tsv_path, part):
             # Find the previous repeat start
             start_times = data[repeat_index]["onset_div"]
             part.add(spt.Repeat(), start=start_times, end=row["onset_div"])
+    part.add(spt.Fine(), start=part.last_point.t)
+    return
 
 
 def read_harmony_tsv(beat_tsv_path, part):
