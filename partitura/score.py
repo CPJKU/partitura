d5a431c0b32c417b8846d37c47e7e3faf7952ee6
melkisedeath <manoskaristineos@gmail.com>
2024-02-13 16:49:14 +0100

    Update Roman Numeral with bass and root.

--- partitura/score.py
+++ partitura/score.py
@@ -15,7 +15,7 @@ from collections.abc import Iterable
 from numbers import Number
 
 # import copy
-from partitura.utils.music import MUSICAL_BEATS, INTERVALCLASSES, INTERVAL_TO_SEMITONES
+from partitura.utils.globals import MUSICAL_BEATS, INTERVALCLASSES, INTERVAL_TO_SEMITONES
 import warnings, sys
 import numpy as np
 import re
@@ -45,8 +45,9 @@ from partitura.utils import (
     _OrderedSet,
     update_note_ids_after_unfolding,
 )
-
 from partitura.utils.generic import interp1d
+from partitura.utils.music import transpose_note, step2pc
+from partitura.utils.globals import (INT_TO_ALT, ALT_TO_INT)
 
 
 class Part(object):
@@ -2780,6 +2781,10 @@ class RomanNumeral(Harmony):
         self.primary_degree = primary_degree if primary_degree is not None else self._process_primary_degree()
         self.secondary_degree = secondary_degree if secondary_degree is not None else self._process_secondary_degree()
         self.quality = quality if quality is not None and quality in self.accepted_qualities else self._process_quality()
+        # only process the root note if the roman numeral is valid
+        if self.local_key and self.primary_degree and self.secondary_degree and self.quality and self.inversion:
+            self.root = self.find_root_note()
+            self.bass_note = self.find_bass_note()
 
     def _process_inversion(self):
         """Find the inversion of the roman numeral from the text"""
@@ -2824,7 +2829,8 @@ class RomanNumeral(Harmony):
         roman_text = self.text.split(":")[-1]
         primary_degree = re.search(r'[a-zA-Z+]+', roman_text)
         if primary_degree:
-            return primary_degree.group(0)
+            # remove o from the text
+            return primary_degree.group(0).replace("o", "")
         return None
 
     def _process_secondary_degree(self):
@@ -2885,6 +2891,47 @@ class RomanNumeral(Harmony):
             warnings.warn(f"Quality for {self.text} was not found, could be a special case. Setting to None.")
             quality = None
         return quality
+
+    def find_root_note(self):
+        """
+        Find the root note of a chord.
+
+        Returns
+        -------
+        number: int
+            The number of the chord.
+        """
+        # Corrected step after degree2
+        interval = Roman2Interval_Min[self.secondary_degree] if self.secondary_degree.islower() else Roman2Interval_Maj[self.secondary_degree]
+        key_step = re.search(r"[a-gA-G]", self.local_key).group(0)
+        key_alter = re.search(r"[#b]", self.local_key).group(0) if re.search(r"[#b]", self.local_key) else ""
+        key_alter = ALT_TO_INT[key_alter]
+        step, alter = transpose_note(key_step, key_alter, interval)
+        # Corrected step after degree1
+        # TODO add support for diminished and augmented chords
+        interval = Roman2Interval_Min[self.primary_degree] if self.primary_degree.islower() else Roman2Interval_Maj[self.primary_degree]
+        step, alter = transpose_note(step, alter, interval)
+        root = step + INT_TO_ALT[alter]
+        return root
+
+    def find_bass_note(self):
+        # TODO add support for diminished and augmented chords
+        step = re.search(r"[a-gA-G]", self.root).group(0)
+        alter = re.search(r"[#b]", self.root)
+        alter = ALT_TO_INT[alter.group(0)] if alter else 0
+
+        if self.inversion == 1:
+            if self.primary_degree.islower():
+                step, alter = transpose_note(step, alter, Interval(3, "m"))
+            else:
+                step, alter = transpose_note(step, alter, Interval(3, "M"))
+        elif self.inversion == 2:
+            step, alter = transpose_note(step, alter, Interval(5, "P"))
+        elif self.inversion == 3:
+            step, alter = transpose_note(step, alter, Interval(7, "m"))
+
+        bass_note_name = step + INT_TO_ALT[alter]
+        return bass_note_name
 
     def __str__(self):
         return f'{super().__str__()} "{self.text}"'
@@ -5161,6 +5208,42 @@ def is_a_within_b(a, b, wholly=False):
     else:
         warnings.warn("a needs to be TimePoint, TimedObject, or int.")
     return contained
+
+
+Roman2Interval_Maj = {
+    "I": Interval(1, "P"),
+    "II": Interval(2, "M"),
+    "III": Interval(3, "M"),
+    "IV": Interval(4, "P"),
+    "V": Interval(5, "P"),
+    "VI": Interval(6, "M"),
+    "VII": Interval(7, "M"),
+    "i": Interval(1, "P"),
+    "ii": Interval(2, "M"),
+    "iii": Interval(3, "m"),
+    "iv": Interval(4, "P"),
+    "v": Interval(5, "P"),
+    "vi": Interval(6, "M"),
+    "vii": Interval(7, "M"),
+}
+
+Roman2Interval_Min = {
+    "I": Interval(1, "P"),
+    "II": Interval(2, "M"),
+    "III": Interval(3, "m"),
+    "IV": Interval(4, "P"),
+    "V": Interval(5, "P"),
+    "VI": Interval(6, "m"),
+    "VII": Interval(7, "m"),
+    "i": Interval(1, "P"),
+    "ii": Interval(2, "M"),
+    "iii": Interval(3, "m"),
+    "iv": Interval(4, "P"),
+    "v": Interval(5, "P"),
+    "vi": Interval(6, "m"),
+    "vii": Interval(7, "m"),
+}
+
 
 
 class InvalidTimePointException(Exception):
