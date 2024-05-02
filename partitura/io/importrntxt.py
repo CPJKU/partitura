8a870477bef86de111dbddadbc9407bba84d1773
melkisedeath <manoskaristineos@gmail.com>
2024-05-02 15:08:36 +0200

    Rntxt parser first draft complete.

--- partitura/io/importrntxt.py
+++ partitura/io/importrntxt.py
@@ -53,13 +53,23 @@ class RntxtParser:
             self.ref_part = score.parts[0]
             quarter_duration = self.ref_part._quarter_durations[0]
             ref_measures = self.ref_part.measures
+            ref_time_sigs = self.ref_part.time_sigs
+            ref_keys = self.ref_part.key_sigs
         else:
             quarter_duration = 4
             ref_measures = []
+            ref_time_sigs = []
+            ref_keys = []
         self.part = spt.Part(id="rn", part_name="Rn", part_abbreviation="rnp", quarter_duration=quarter_duration)
         # include measures
         for measure in ref_measures:
             self.part.add(measure, measure.start.t, measure.end.t)
+        # include time signatures
+        for time_sig in ref_time_sigs:
+            self.part.add(time_sig, time_sig.start.t)
+        # include key signatures
+        for key in ref_keys:
+            self.part.add(key, key.start.t)
         self.measures = {m.number: m for m in self.part.measures}
         self.current_measure = None
         self.current_position = 0
@@ -84,19 +94,34 @@ class RntxtParser:
             elif line.startswith("m"):
                 self._handle_measure(line)
 
+        self.currate_ending_times()
+
+    def currate_ending_times(self):
+        romans = list(self.part.iter_all(spt.RomanNumeral))
+        starting_times = [rn.start.t for rn in romans]
+        argsort_start = np.argsort(starting_times)
+        for i, rn_idx in enumerate(argsort_start[:-1]):
+            rn = romans[rn_idx]
+            if rn.end is None:
+                rn.end = romans[argsort_start[i+1]].start
+
     def _handle_measure(self, line):
         if not self._validate_measure_line(line):
             return
         elements = line.split(" ")
         measure_number = elements[0].strip("m")
         if not measure_number.isnumeric():
-            # TODO: check if it is a valid measure number or variation
-            raise ValueError(f"Invalid measure number: {measure_number}")
+            # TODO: complete check for variation measures
+            if "var" in measure_number:
+                return
+            else:
+
+                raise ValueError(f"Invalid measure number: {measure_number}")
         measure_number = int(measure_number)
         if measure_number not in self.measures.keys():
             self.current_measure = spt.Measure(number=measure_number)
             self.measures[measure_number] = self.current_measure
-            self.part.add(self.current_measure, self.current_position)
+            self.part.add(self.current_measure, int(self.current_position))
         else:
             self.current_measure = self.measures[measure_number]
 
@@ -114,9 +139,9 @@ class RntxtParser:
                 if (self.current_position == 0 and self.num_parsed_romans == 0):
                     self.current_position = 0
                 else:
-                    self.current_position = self.part.inv_beat_map(self.part.beat_map(self.current_position) + self.measure_beat_position - 1)
+                    self.current_position = self.part.inv_beat_map(self.part.beat_map(self.current_position) + self.measure_beat_position - 1).item()
             else:
-                self.current_position = self.part.inv_beat_map(self.part.beat_map(self.current_measure.start.t) + self.measure_beat_position - 1)
+                self.current_position = self.part.inv_beat_map(self.part.beat_map(self.current_measure.start.t) + self.measure_beat_position - 1).item()
 
         # if element starts with [A-G] and it includes : it is a key
         elif element[0] in "ABCDEFG" and ":" in element:
@@ -140,15 +165,28 @@ class RntxtParser:
         fifths, mode = key_name_to_fifths_mode(element.strip(":"))
         ks = spt.KeySignature(fifths=fifths, mode=mode)
         self.key = element.strip(":")
-        self.part.add(ks, self.current_position)
+        self.part.add(ks, int(self.current_position))
 
     def _handle_barline(self, element):
         pass
 
     def _handle_roman_numeral(self, element):
         element = element.strip()
+        # change RN6/5 to RN65
+        if "/" in element:
+            if element.split("/")[1][0].isnumeric():
+                # replace only the first occurrence of "/" with ""
+                element = element.replace("/", "", 1)
         rn = spt.RomanNumeral(text=element, local_key=self.key)
-        self.part.add(rn, self.current_position)
+
+        try:
+            self.part.add(rn, int(self.current_position))
+        except ValueError:
+            print(f"Could not add roman numeral {element} at position {self.current_position}")
+            return
+        # Set the end of the previous roman numeral
+        # if self.previous_rn is not None:
+        #     self.previous_rn.end = spt.TimePoint(t=self.current_position)
         self.num_parsed_romans += 1
 
     def _validate_measure_line(self, line):
