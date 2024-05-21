c1e27f7553d9c9a636f4b74c2e7445d40e38281e
melkisedeath <manoskaristineos@gmail.com>
2024-05-21 17:46:36 +0200

    fixing test file typo.

--- tests/__init__.py
+++ tests/__init__.py
@@ -183,7 +183,7 @@ KERN_TESTFILES = [
         "double_repeat_example.krn",
         "fine_with_repeat.krn",
         "tuple_durations.krn",
-        "voice_dublications.krn",
+        "voice_duplication.krn",
         "variable_length_pr_bug.krn",
         "chor228.krn",
     ]
