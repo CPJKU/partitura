2095af5547424b6a983609b8d50d664f771bdd25
melkisedeath <manoskaristineos@gmail.com>
2024-07-17 17:04:41 +0200

    updated tests with an assertion

--- tests/test_dcml_import.py
+++ tests/test_dcml_import.py
@@ -2,6 +2,7 @@ import unittest
 from partitura import load_dcml
 from tests import TSV_PATH
 import os
+import pandas as pd
 
 
 class ImportDCMLAnnotations(unittest.TestCase):
@@ -10,7 +11,9 @@ class ImportDCMLAnnotations(unittest.TestCase):
         measure_path = os.path.join(TSV_PATH, "test_measures.tsv")
         harmony_path = os.path.join(TSV_PATH, "test_harmonies.tsv")
         score = load_dcml(note_path, measure_path, harmony_path)
+        note_lines = pd.read_csv(note_path, sep="\t", header=None)
         self.assertEqual(len(score.parts), 1)
+        self.assertEqual(len(score[0].notes), len(note_lines)-1, "Number of notes do not match")
 
 
 if __name__ == '__main__':
