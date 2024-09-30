ffa1b43bb68e004f279f82d764eca576682a316a
sildater <silvandavidpeter@gmail.com>
2024-09-30 08:57:31 +0200

    note array to part header

--- partitura/musicanalysis/note_array_to_score.py
+++ partitura/musicanalysis/note_array_to_score.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This module contains methods to create parts from note arrays.
+"""
 from partitura.score import ScoreLike, Part
 from partitura.utils import (
     estimate_symbolic_duration,
