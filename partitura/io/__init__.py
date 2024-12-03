19cf16472c408a37a15bbd344a9e6d0d644549f2
melkisedeath <manoskaristineos@gmail.com>
2024-12-03 12:31:20 +0100

    Removed rntxt refs.

--- partitura/io/__init__.py
+++ partitura/io/__init__.py
@@ -14,7 +14,6 @@ from .musescore import load_via_musescore
 from .importmatch import load_match
 from .importmei import load_mei
 from .importkern import load_kern
-from .importrntxt import load_rntxt
 from .exportkern import save_kern
 from .importparangonada import load_parangonada_csv
 from .exportparangonada import save_parangonada_csv
